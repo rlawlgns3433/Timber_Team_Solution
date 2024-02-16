2acfb2cdb341d533d6e75737b5df372a2ed69c50
장수진 <jsooj115@gmail.com>
2024-02-17 01:34:19 +0900

    selectMode에서 BGM 추가, text 출력 위치 조정

--- Framework/SceneSelectMode.cpp
+++ Framework/SceneSelectMode.cpp
@@ -17,7 +17,7 @@ void SceneSelectMode::Init()
 	ResourceManager<sf::Font>& resourceManager = ResourceManager<sf::Font>::Instance();
 
 	background = new SpriteGo("background");
-	background->SetTexture(*textureManager.GetResource("graphics/background.png")); //origin, position 설정?
+	background->SetTexture(*textureManager.GetResource("graphics/background.png"));
 	AddGameObject(background);
 
 	pressNumber = new TextGo("pressNumber");
@@ -29,13 +29,13 @@ void SceneSelectMode::Init()
 	num1ForSinglePlay = new TextGo("num1ForSinglePlay");
 	num1ForSinglePlay->Set(*fontManager.GetResource("fonts/KOMIKAP_.ttf"), "Num1 For Single Play", 70, sf::Color::White);
 	num1ForSinglePlay->SetOrigin(Origins::MC);
-	num1ForSinglePlay->SetPosition({ 1920 / 4, 1080 / 3 });
+	num1ForSinglePlay->SetPosition(FRAMEWORK.GetWindowSize().x * (1.f / 4.f), FRAMEWORK.GetWindowSize().y * 0.6f);
 	AddGameObject(num1ForSinglePlay);
 
 	num2ForDuoPlay = new TextGo("num2ForDuoPlay");
 	num2ForDuoPlay->Set(*fontManager.GetResource("fonts/KOMIKAP_.ttf"), "Num2 For Duo Play", 70, sf::Color::White);
 	num2ForDuoPlay->SetOrigin(Origins::MC);
-	num2ForDuoPlay->SetPosition({ 1920 / 3, 1080 / 3 });
+	num2ForDuoPlay->SetPosition(FRAMEWORK.GetWindowSize().x * (3.f / 4.f), FRAMEWORK.GetWindowSize().y * 0.6f);
 	AddGameObject(num2ForDuoPlay);
 
 	for (GameObject* obj : gameObjects)
@@ -57,6 +57,7 @@ void SceneSelectMode::Release()
 
 void SceneSelectMode::Enter()
 {
+	SCENEMANAGER.PlayBGM();
 	Scene::Enter();
 }
 
@@ -71,13 +72,15 @@ void SceneSelectMode::Update(float dt)
 
 	if (InputManager::GetKeyDown(sf::Keyboard::Num1))
 	{
+		SCENEMANAGER.StopBGM();
 		//TO-Do : 어떠한 모드 선택했다는 것 selectCharacter씬에 알려줄 수 있는 함수 SceneManager에 bool형으로 만들기
 		SceneManager::Instance().ChangeScene(SceneIDs::SceneSelectCharacter);
 	}
 
 	if (InputManager::GetKeyDown(sf::Keyboard::Num2))
 	{
-		
+		SCENEMANAGER.StopBGM();
+
 		SceneManager::Instance().ChangeScene(SceneIDs::SceneSelectCharacter);
 	}
 }
