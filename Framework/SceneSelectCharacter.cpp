e7983a1dfbf166bd9e81e68293abe085a971f466
kim_Bo <qhgus5459@naver.com>
2024-02-17 01:20:27 +0900

    player2 위치 수정

--- Framework/SceneSelectCharacter.cpp
+++ Framework/SceneSelectCharacter.cpp
@@ -32,11 +32,11 @@ void SceneSelectCharacter::Init()
 	spritePlayer1->SetPosition({1920*0.3,1080*0.7});
 	AddGameObject(spritePlayer1);
 
-	/*spritePlayer2 = new SpriteGo("plater2");                                     //아직 player2가 로드 안됨
+	spritePlayer2 = new SpriteGo("plater2");                                     
 	spritePlayer2->SetTexture(*textureManager.GetResource("graphics/player2.png"));
 	spritePlayer2->SetOrigin(Origins::BC);
-	spritePlayer2->SetPosition({ 1920 * 0.7,1080 * 0.5 });
-	AddGameObject(spritePlayer2);*/
+	spritePlayer2->SetPosition({ 1920 * 0.7,1080 * 0.7 });
+	AddGameObject(spritePlayer2);
 
 	SelectText = new TextGo("Num1 Character");
 	SelectText->Set(*fontManager.GetResource("fonts/KOMIKAP_.ttf"), "Num1 : Old WoodCutter", 30, sf::Color::Black);
@@ -47,7 +47,7 @@ void SceneSelectCharacter::Init()
 	SelectText = new TextGo("Num2 Character");
 	SelectText->Set(*fontManager.GetResource("fonts/KOMIKAP_.ttf"), "Num2 : young WoodCutter", 30, sf::Color::Black);
 	SelectText->SetOrigin(Origins::MC);
-	SelectText->SetPosition(FRAMEWORK.GetWindowSize().x * 0.6, FRAMEWORK.GetWindowSize().y * 0.8 - 100);
+	SelectText->SetPosition(FRAMEWORK.GetWindowSize().x * 0.7, FRAMEWORK.GetWindowSize().y * 0.8 - 100);
 	AddGameObject(SelectText);
 
 	
@@ -76,7 +76,7 @@ void SceneSelectCharacter::Update(float dt)                              //1번 o
 
 	if (InputManager::GetKeyDown(sf::Keyboard::Num1))                    //SelectMod에서 1인, 2인 어떤걸 선택했는지 어떻게 체크하지?
 	{
-		SCENEMANAGER.SetPlayerOneSelect(1);                              //
+		SCENEMANAGER.SetPlayerOneSelect(1);                              
 		SceneManager::Instance().ChangeScene(SceneIDs::SceneGameSingle); //이렇게 하면 1인 모드로 밖에 못가는데 어쩌지???
 																		
 	}
